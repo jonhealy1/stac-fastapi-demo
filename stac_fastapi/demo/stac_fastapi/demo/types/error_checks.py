6ae35e339bae316805fe2a9c94d4ba7b846f2de8
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-11 07:49:51 -0700

    remove session from error checks

--- stac_fastapi/demo/stac_fastapi/demo/types/error_checks.py
+++ stac_fastapi/demo/stac_fastapi/demo/types/error_checks.py
@@ -3,7 +3,6 @@
 import attr
 from pymongo import MongoClient
 
-from stac_fastapi.demo.session import Session
 from stac_fastapi.types import stac as stac_types
 from stac_fastapi.types.errors import ConflictError, ForeignKeyError, NotFoundError
 
@@ -11,46 +10,42 @@ from stac_fastapi.types.errors import ConflictError, ForeignKeyError, NotFoundEr
 @attr.s
 class ErrorChecks:
     """error checks class."""
-
-    session: Session = attr.ib(default=Session)
     client: MongoClient = attr.ib(default=None)
 
-    def _check_collection_foreign_key(self, model: stac_types.Collection):
+    def check_collection_foreign_key(self, model: stac_types.Collection):
         if not self.client.stac.stac_collection.count_documents(
-            {"id": model["collection"]}, limit=1, session=self.session
+            {"id": model["collection"]}, limit=1
         ):
             raise ForeignKeyError(f"Collection {model['collection']} does not exist")
 
-    def _check_collection_conflict(self, model: stac_types.Collection):
+    def check_collection_conflict(self, model: stac_types.Collection):
         if self.client.stac.stac_collection.count_documents(
-            {"id": model["id"]}, limit=1, session=self.session
+            {"id": model["id"]}, limit=1
         ):
             raise ConflictError(f"Collection {model['id']} already exists")
 
-    def _check_collection_not_found(self, collection_id: str):
+    def check_collection_not_found(self, collection_id: str):
         if (
             self.client.stac.stac_collection.count_documents(
-                {"id": collection_id}, session=self.session
+                {"id": collection_id}
             )
             == 0
         ):
             raise NotFoundError(f"Collection {collection_id} not found")
 
-    def _check_item_conflict(self, model: stac_types.Item):
+    def check_item_conflict(self, model: stac_types.Item):
         if self.client.stac.stac_item.count_documents(
             {"id": model["id"], "collection": model["collection"]},
-            limit=1,
-            session=self.session,
+            limit=1
         ):
             raise ConflictError(
                 f"Item {model['id']} in collection {model['collection']} already exists"
             )
 
-    def _check_item_not_found(self, item_id: str, collection_id: str):
+    def check_item_not_found(self, item_id: str, collection_id: str):
         if (
             self.client.stac.stac_item.count_documents(
-                {"id": item_id, "collection": collection_id},
-                session=self.session,
+                {"id": item_id, "collection": collection_id}
             )
             == 0
         ):
