fc1ae5f4119dabe6f0f34875fb1c3a212f911a97
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-11 07:08:24 -0700

    clean upu config

--- stac_fastapi/demo/stac_fastapi/demo/config.py
+++ stac_fastapi/demo/stac_fastapi/demo/config.py
@@ -1,8 +1,7 @@
 """API configuration."""
 import os
-from typing import Set
 
-from pymongo import GEO2D, GEOSPHERE, MongoClient, errors
+from pymongo import GEOSPHERE, MongoClient, errors
 
 from stac_fastapi.types.config import ApiSettings
 
@@ -12,12 +11,6 @@ PORT = os.getenv("MONGO_PORT")
 
 class MongoSettings(ApiSettings):
     """API settings."""
-
-    # # Fields which are defined by STAC but not included in the database model
-    # forbidden_fields: Set[str] = {"type"}
-
-    # # Fields which are item properties but indexed as distinct fields in the database model
-    # indexed_fields: Set[str] = {"datetime"}
 
     @property
     def create_client(self):
@@ -38,7 +31,6 @@ class MongoSettings(ApiSettings):
             item_table.create_index([("properties.created", 1)])
             item_table.create_index([("properties.updated", 1)])
             item_table.create_index([("bbox", GEOSPHERE)])
-            item_table.create_index([("bbox", GEO2D)])
 
         except errors.ServerSelectionTimeoutError as err:
             client = None
