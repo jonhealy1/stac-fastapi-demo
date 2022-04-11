b258ef46cd7058cbac3e888fc227ed6d2ecd68ea
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-11 05:40:57 -0700

    clean up config

--- stac_fastapi/demo/stac_fastapi/demo/config.py
+++ stac_fastapi/demo/stac_fastapi/demo/config.py
@@ -13,11 +13,11 @@ PORT = os.getenv("MONGO_PORT")
 class MongoSettings(ApiSettings):
     """API settings."""
 
-    # Fields which are defined by STAC but not included in the database model
-    forbidden_fields: Set[str] = {"type"}
+    # # Fields which are defined by STAC but not included in the database model
+    # forbidden_fields: Set[str] = {"type"}
 
-    # Fields which are item properties but indexed as distinct fields in the database model
-    indexed_fields: Set[str] = {"datetime"}
+    # # Fields which are item properties but indexed as distinct fields in the database model
+    # indexed_fields: Set[str] = {"datetime"}
 
     @property
     def create_client(self):
